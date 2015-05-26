ab015662b11ee520b6b24a9c9cf60863e55b4f90
Kota Mizushima <mizukota@gmail.com>
2015-05-26 21:28:52 +0900

    Implement List data type

--- list.rs
+++ list.rs
@@ -7,16 +7,20 @@ enum List<T> {
   Nil
 }
 
-impl<T> fmt::Display for List<T : Display> {
+impl<T> fmt::Display for List<T> where T : fmt::Display {//TがDisplay traitを実装していることを保証したい
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match *self {
-      Cons(ref value, ref tail) => write!(f, "{v} :: Nil", v = value),
+      Cons(ref head, ref tail) => 
+        {
+          write!(f, "{head} ::", head = head);
+          write!(f, "{tail}", tail = tail)
+        }
       Nil => write!(f, "Nil")
     }
   }
 }
 
 fn main() {
-  let list = Cons("A", Box::new(Cons("B", Box::new(Nil))));
-  println!("Hello, :?{rust}", rust=list);
+  let list = Cons("A", Box::new(Cons("B", Box::new(Cons("C", Box::new(Nil))))));
+  println!("Hello, {rust}", rust=list);
 }
